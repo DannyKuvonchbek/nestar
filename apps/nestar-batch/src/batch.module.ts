b5ca940578dda8fb7bd25b61f7282993a0fba6d6
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-09-07 20:59:27 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedulers

--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -3,8 +3,9 @@ import { BatchController } from './batch.controller';
 import { BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
 @Module({
-	imports: [ConfigModule.forRoot(), DatabaseModule],
+	imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
 	controllers: [BatchController],
 	providers: [BatchService],
 })
